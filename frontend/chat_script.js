d35996c66f5e6d3e56ca9db8eab1accd3726537f
Tenerec123 <alvarofer8a@gmail.com>
2026-05-15 19:23:51 +0200

    Habit tracker table without functionality for routines

--- frontend/chat_script.js
+++ frontend/chat_script.js
@@ -59,20 +59,12 @@ async function ToggleMic() {
         }
         if (can_record){
             recorder.start();
-            mic.innerHTML = `
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mic-fill" viewBox="0 0 16 16">
-                    <path d="M5 3a3 3 0 0 1 6 0v5a3 3 0 0 1-6 0z"/>
-                    <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5"/>
-                </svg>`
+            mic.innerHTML = `<i class="bi bi-mic-fill"></i>`
         }
         
     } else {
         recorder.stop();
-        mic.innerHTML = `
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mic" viewBox="0 0 16 16">
-                <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5"/>
-                <path d="M10 8a2 2 0 1 1-4 0V3a2 2 0 1 1 4 0zM8 0a3 3 0 0 0-3 3v5a3 3 0 0 0 6 0V3a3 3 0 0 0-3-3"/>
-            </svg>`
+        mic.innerHTML = `<i class="bi bi-mic"></i>`
     }
 }
 mic.addEventListener('click', ToggleMic);
@@ -215,9 +207,7 @@ async function getConversations(){
             divConv.innerHTML = `
                 <button class="conv-select" onclick="loadConversation(${conversation.id}, ${i})">${conversation.title}</button>
                 <button class="conv-options" onclick="OpenMenu(this, ${conversation.id}, ${i})">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
-                        <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3"/>
-                    </svg>
+                    <i class="bi bi-three-dots"></i>
                 </button>
             `;
             // Añadimos este nuevo div al contenedor principal
